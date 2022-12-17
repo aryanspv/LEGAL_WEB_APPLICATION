9f9b4ba0dd5a2f6b119596bb58fcf11559db7272
Aryan Sharma <64528511+aryanspv@users.noreply.github.com>
2022-12-18 05:02:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ with body:
     upload_col.header('Upload text file here')
     uploaded_file = upload_col.file_uploader(
         'Choose your .txt file', type="txt")
-
+    rawtext = uploaded_file
     no_of_lines = st.slider("Select number of lines in summary", 3, 5, 7)
     if st.button('Get Summary'):
         with summary_container:
